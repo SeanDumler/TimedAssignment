390c31b2dec4e24dd86d7804314fc9aab9924a9e
SeanDumler <sean.dumler@outlook.com>
2023-06-19 18:47:40 -0500

    Post created

--- TimedAssignment.Data/Post.cs
+++ TimedAssignment.Data/Post.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,29 @@ namespace TimedAssignment.Data
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Title cannot exceed 50 characters.")]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(300, ErrorMessage = "Text cannot exceed 300 characters.")]
+        public string Text { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; } = null!;
+
+        public int CommentId { get; set; }
+        
+        [ForeignKey(nameof(CommentId))]
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public int LikeId { get; set; }
+
+        [ForeignKey(nameof(LikeId))]
+        public virtual List<Like> Likes { get; set; } = new List<Like>();
         
     }
 }
--- TimedAssignment.Services/PostServices/PostService.cs
+++ TimedAssignment.Services/PostServices/PostService.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimedAssignment.Models.Posts;
 
 namespace TimedAssignment.Services.PostServices
 {
     public class PostService : IPostService
     {
-        
+        public Task<bool> AddPost(PostCreate model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDetail> GetPostById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PostListItem>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePost(PostEdit model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
