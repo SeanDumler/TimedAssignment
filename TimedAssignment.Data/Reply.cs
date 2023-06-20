c9029cbcca8f1d64a47770b7d70803bd0c7b9c88
Kevin Michael <kcamichael@gmail.com>
2023-06-20 03:41:56 -0400

    main

--- TimedAssignment.Data/Reply.cs
+++ TimedAssignment.Data/Reply.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,14 @@ namespace TimedAssignment.Data
 {
     public class Reply
     {
-        
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(512)]
+        [ForeignKey(nameof(UserId))]
+        public virtual Reply UserId { get; set; }
+        public Guid AuthorId { get; set; }
+        [Required]
+        [MaxLength(512)]
+        public string Text { get; set; }
     }
 }
