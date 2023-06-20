c768640d5deb958ab7d48037044788a4a0b0005d
Kevin Michael <kcamichael@gmail.com>
2023-06-20 04:04:11 -0400

    Kevin

--- TimedAssignment.Services/ReplyServices/IReplyService.cs
+++ TimedAssignment.Services/ReplyServices/IReplyService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimedAssignment.Models.Replies;
 
 namespace TimedAssignment.Services.ReplyServices
 {
     public interface IReplyService
     {
-        
+        Task<bool> AddReply(ReplyCreate model);
+        Task<bool> UpdateReply(ReplyEdit model);
+        Task<bool> DeleteReply(int id);
+        Task<ReplyDetail> GetReplyByCommentId(int id);
+        Task<ReplyDetail> GetReplyById(int id);
     }
 }
